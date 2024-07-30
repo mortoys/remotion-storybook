cfbcac187f7c4a2a6cca88c3f4d232f815fb3f17
Morty Lu <mortywork@outlook.com>
2024-07-30 15:10:39 +0800

    .

---
+++ components/Root.tsx
@@ -0,0 +1,6 @@
+
+export const MyComposition = () => {
+    return null;
+};
+
+
---
+++ stories/Player.stories.tsx
@@ -0,0 +1,51 @@
+
+
+import type { Meta, StoryObj } from "@storybook/react";
+import { Player } from "@remotion/player";
+
+import { MyComposition } from '@/components/Root'
+
+const VIDEO_WIDTH = 1024;
+const VIDEO_HEIGHT = 1024;
+const VIDEO_FPS = 30;
+const DURATION_IN_FRAMES = VIDEO_FPS * 4;
+
+export const MyPlayer = () => 
+    <Player
+        component={MyComposition}
+        // inputProps={inputProps}
+        durationInFrames={DURATION_IN_FRAMES}
+        fps={VIDEO_FPS}
+        compositionHeight={VIDEO_HEIGHT}
+        compositionWidth={VIDEO_WIDTH}
+        style={{
+            width: "300px",
+        }}
+        // controls
+        autoPlay
+        loop
+    />
+
+const meta = {
+  title: "Slide/Player",
+  component: MyPlayer,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    // layout: "fullscreen",
+    layout: 'centered'
+  },
+  args: {
+  },
+} satisfies Meta<typeof MyPlayer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// export const LoggedIn: Story = {
+//   args: {
+//   },
+// };
+
+export const LoggedOut: Story = {};
