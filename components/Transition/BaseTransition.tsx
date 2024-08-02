509344556fd1270bb39951e9e78ee93f6d1e5c6f
Morty Lu <mortywork@outlook.com>
2024-08-02 18:58:10 +0800

    .

--- components/Transition/BaseTransition.tsx
+++ components/Transition/BaseTransition.tsx
@@ -1,10 +1,10 @@
 import React from "React";
 import { useVideoConfig } from "remotion";
-import {
+// import {
   // linearTiming,
   // springTiming,
-  TransitionSeries,
-} from "@remotion/transitions";
+  // TransitionSeries,
+// } from "@remotion/transitions";
 import { AbsoluteFill } from "remotion";
 
 // import { none } from "@remotion/transitions/none";
@@ -29,53 +29,54 @@ const Fill = ({ color, text }: { color: string, text: string }) => {
 export const TransitionComponent = () => {
   const { width, height } = useVideoConfig();
   return (
-    <TransitionSeries>
-      <TransitionSeries.Sequence durationInFrames={60}>
-        <Fill color="blue" text="none" />
-      </TransitionSeries.Sequence>
-      {/* <TransitionSeries.Transition
-        timing={linearTiming({ durationInFrames: 10 })}
-        presentation={none()}
-      />
-      <TransitionSeries.Sequence durationInFrames={60}>
-        <Fill color="red" text="slide" />
-      </TransitionSeries.Sequence>
-      <TransitionSeries.Transition
-        timing={linearTiming({ durationInFrames: 10 })}
-        presentation={slide()}
-      />
-      <TransitionSeries.Sequence durationInFrames={60}>
-        <Fill color="green" text="fade" />
-      </TransitionSeries.Sequence>
-      <TransitionSeries.Transition
-        timing={linearTiming({ durationInFrames: 10 })}
-        presentation={fade()}
-      />
-      <TransitionSeries.Sequence durationInFrames={60}>
-        <Fill color="black" text="flip" />
-      </TransitionSeries.Sequence>
-      <TransitionSeries.Transition
-        timing={linearTiming({ durationInFrames: 10 })}
-        presentation={flip()}
-      />
-      <TransitionSeries.Sequence durationInFrames={60}>
-        <Fill color="yellow" text="clockWipe" />
-      </TransitionSeries.Sequence>
-      <TransitionSeries.Transition
-        timing={linearTiming({ durationInFrames: 10 })}
-        presentation={clockWipe({ width, height })}
-      />
-      <TransitionSeries.Sequence durationInFrames={60}>
-        <Fill color="green" text="wipe" />
-      </TransitionSeries.Sequence>
-      <TransitionSeries.Transition
-        timing={linearTiming({ durationInFrames: 10 })}
-        presentation={wipe()}
-      />
-      <TransitionSeries.Sequence durationInFrames={60}>
-        <Fill color="red" text="end" />
-      </TransitionSeries.Sequence> */}
-    </TransitionSeries>
+    null
+    // <TransitionSeries>
+    //   <TransitionSeries.Sequence durationInFrames={60}>
+    //     <Fill color="blue" text="none" />
+    //   </TransitionSeries.Sequence>
+    //   <TransitionSeries.Transition
+    //     timing={linearTiming({ durationInFrames: 10 })}
+    //     presentation={none()}
+    //   />
+    //   <TransitionSeries.Sequence durationInFrames={60}>
+    //     <Fill color="red" text="slide" />
+    //   </TransitionSeries.Sequence>
+    //   <TransitionSeries.Transition
+    //     timing={linearTiming({ durationInFrames: 10 })}
+    //     presentation={slide()}
+    //   />
+    //   <TransitionSeries.Sequence durationInFrames={60}>
+    //     <Fill color="green" text="fade" />
+    //   </TransitionSeries.Sequence>
+    //   <TransitionSeries.Transition
+    //     timing={linearTiming({ durationInFrames: 10 })}
+    //     presentation={fade()}
+    //   />
+    //   <TransitionSeries.Sequence durationInFrames={60}>
+    //     <Fill color="black" text="flip" />
+    //   </TransitionSeries.Sequence>
+    //   <TransitionSeries.Transition
+    //     timing={linearTiming({ durationInFrames: 10 })}
+    //     presentation={flip()}
+    //   />
+    //   <TransitionSeries.Sequence durationInFrames={60}>
+    //     <Fill color="yellow" text="clockWipe" />
+    //   </TransitionSeries.Sequence>
+    //   <TransitionSeries.Transition
+    //     timing={linearTiming({ durationInFrames: 10 })}
+    //     presentation={clockWipe({ width, height })}
+    //   />
+    //   <TransitionSeries.Sequence durationInFrames={60}>
+    //     <Fill color="green" text="wipe" />
+    //   </TransitionSeries.Sequence>
+    //   <TransitionSeries.Transition
+    //     timing={linearTiming({ durationInFrames: 10 })}
+    //     presentation={wipe()}
+    //   />
+    //   <TransitionSeries.Sequence durationInFrames={60}>
+    //     <Fill color="red" text="end" />
+    //   </TransitionSeries.Sequence>
+    // </TransitionSeries>
   );
 };
 
