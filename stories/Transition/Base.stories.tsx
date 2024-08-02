0c95a44f4eaf8782e93fc22d0e72e7608dc8b07c
Morty Lu <mortywork@outlook.com>
2024-08-02 18:39:22 +0800

    .

--- stories/Transition/Base.stories.tsx
+++ stories/Transition/Base.stories.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import type { Meta, StoryObj } from "@storybook/react";
 
 import TransitionComponent from "@/components/Transition/BaseTransition";
-// import { TextProps } from "@/components/Transition/Text";
-// import type { Mode } from "@/components/Subtitle/Text";
 
 import Player from "../Player";
 
@@ -19,18 +17,10 @@ const meta = {
   parameters: {
     layout: "centered",
   },
-  argTypes: {
-  },
 } satisfies Meta<typeof Component>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Short: Story = {
-  args: {
-    size: 20,
-    bold: true,
-    // text: '这是一个测试 subtitle 的英文混合。',
-    text: 'Artificial Intelligence (AI)'
-  },
 };
